aa2dd28979c0bc17b54cc88ddcada2ec5bc57523
ABHISHEK PANDA <official.abhishekpanda@gmail.com>
2026-08-29 20:13:32 +0530

    feat(careeros): route CareerOS landing from StackedIN

--- main.jsx
+++ main.jsx
@@ -1,9 +1,13 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import Dashboard from "./substack_bw_dashboard.jsx";
+import CareerOSLanding from "./src/careeros/CareerOSLanding.jsx";
+
+const route = window.location.pathname.replace(/^\/+|\/+$/g, "").toLowerCase();
+const isCareerOS = route === "careeros" || route === "career-os" || route === "career";
 
 createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <Dashboard />
+    {isCareerOS ? <CareerOSLanding /> : <Dashboard />}
   </React.StrictMode>,
 );
